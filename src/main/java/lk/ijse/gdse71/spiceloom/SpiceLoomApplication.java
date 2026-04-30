91504f84673e91fc64e8265fcad8f8f185c047a8
Sumuditha-Janith <sumuditha356@gmail.com>
2026-05-01 04:38:08 +0530

    Initial Commit

---
+++ src/main/java/lk/ijse/gdse71/spiceloom/SpiceLoomApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse71.spiceloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiceLoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiceLoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/gdse71/spiceloom/SpiceLoomApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse71.spiceloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiceLoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
